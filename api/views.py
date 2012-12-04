d2593b32f5caf8988fd7333ce584af1536b8dbb2
Yann <yann.savary@gmail.com>
2012-12-04 23:29:51 +0100

    Django 1.5

---
+++ api/views.py
@@ -0,0 +1,31 @@
+import os
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from pymongo import Connection, uri_parser
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'List': reverse('station-list', request=request),
+        'Mauborget': reverse('station-detail', ['jdc_1001'], request=request),
+        'Zinal': reverse('station-detail', ['jdc_1003'], request=request),
+    })
+
+
+@api_view(['GET'])
+def station_list(request):
+    if request.method == 'GET':
+        return Response(mongo_db.stations.find())
+
+
+@api_view(['GET'])
+def station_detail(request, id):
+    if request.method == 'GET':
+        return Response(mongo_db.stations.find_one(id))
+
+
+mongo_url = os.environ['WINDMOBILE_MONGO_URL']
+connection = Connection(mongo_url)
+db_name = uri_parser.parse_uri(mongo_url)['database']
+mongo_db = connection[db_name]

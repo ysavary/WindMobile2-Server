d46a77fd6a5c019cbed74d9e3d8c8fa62559f889
Yann <yann.savary@gmail.com>
2013-03-17 15:55:48 +0100

    Fixed flock script

--- template.sh
+++ template.sh
@@ -1,14 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # Makes sure we exit if flock fails.
 set -e
 
 (
-  # Wait for lock on /var/lock/.myscript.exclusivelock (fd 200) for 5 seconds
-  flock -x -w 5 200
+  flock -n 200 || exit 1
 
-  # Do stuff
-  cd /home/windmobile
-  python jdc.py 2> log/jdc.err
+  envdir /home/WindMobile2-Server/envdir python /home/WindMobile2-Server/providers/provider.py 2>/var/log/windmobile/provider.err
 
-) 200>/home/windmobile/jdc.lock
+) 200>/home/WindMobile2-Server/provider.lock

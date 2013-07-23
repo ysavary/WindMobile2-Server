47f3995a4a003b08acc150058515906fde2eb7fb
Yann <yann.savary@gmail.com>
2013-07-23 22:36:19 +0200

    Using windmobile instead the root user

--- provider.template.sh
+++ provider.template.sh
@@ -7,6 +7,6 @@ PROVIDER=provider
 (
   flock -n 200 || exit 1
 
-  envdir /root/WindMobile2-Server/envdir /root/.virtualenvs/windmobile/bin/python /root/WindMobile2-Server/providers/$PROVIDER.py 1>/dev/null 2>/var/log/windmobile/$PROVIDER.err
+  envdir /home/windmobile/WindMobile2-Server/envdir /root/.virtualenvs/windmobile/bin/python /home/windmobile/WindMobile2-Server/providers/$PROVIDER.py 1>/dev/null 2>/var/log/windmobile/$PROVIDER.err
 
-) 200>/root/WindMobile2-Server/$PROVIDER.lock
+) 200>/home/windmobile/WindMobile2-Server/$PROVIDER.lock

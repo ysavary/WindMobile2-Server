a31505cadb843fbd84640192734f9b0bd2b9f064
Yann <yann.savary@gmail.com>
2013-08-27 22:02:15 +0200

    Added python envdir

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import envdir
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "windmobile.settings")
+    envdir.read('envdir')
 
     from django.core.management import execute_from_command_line
 

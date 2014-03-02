817d1d10b4ce044bb5435b41b2f6ca9421a71b0f
Yann <yann.savary@gmail.com>
2014-03-02 17:54:41 +0100

    Stations highlight with snapsvg

--- windmobile/web/static/web/js/windmobile.js
+++ windmobile/web/static/web/js/windmobile.js
@@ -1,4 +1,4 @@
-var module = angular.module('WindMobileModule', ['google-maps'], function ($interpolateProvider) {
+var app = angular.module('WindMobileApp', ['snap', 'google-maps'], function ($interpolateProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
     })
@@ -11,7 +11,7 @@ var module = angular.module('WindMobileModule', ['google-maps'], function ($inte
             }
         }
     })
-    .directive('minichart', function () {
+    .directive('miniChart', function () {
         return {
             restrict: "E",
             scope: {
@@ -22,19 +22,71 @@ var module = angular.module('WindMobileModule', ['google-maps'], function ($inte
                     attrs.$observe('data', function (newValue) {
                         element.html(newValue);
                         element.sparkline('html', {
+                            width: '60px',
+                            height: '20px',
                             type: 'line',
                             spotColor: false,
                             minSpotColor: false,
                             maxSpotColor: false,
-                            width: '40px'
+                            lineColor: '#fff',
+                            fillColor: '#444'
                         });
                     });
                 };
             }
         };
+    })
+    .directive('windDirection', function () {
+        return {
+            restrict: "A",
+            compile: function (tElement, tAttrs, transclude) {
+                return function (scope, element, attrs) {
+                    scope.$watch('historic', function(newValue, oldValue) {
+                        var width = parseFloat($(element[0]).width());
+                        var height = parseFloat($(element[0]).height());
+
+                        var radius = Math.min(width, height) / 2;
+
+                        var paper = Snap(element[0]);
+                        var circle = paper.circle(width/2, height/2, radius);
+                        circle.attr({
+                            stroke: "#fff",
+                            strokeWidth: 2
+                        });
+
+                        // The center
+                        var lastX = width/2;
+                        var lastY = width/2;
+
+                        var currentRadius = 0.0;
+                        for (var i = 0; i < scope.historic.length; i++) {
+                            var direction = scope.historic[i]['w-dir'];
+
+                            currentRadius += radius / scope.historic.length;
+                            var directionRadian = (direction + 90) * (Math.PI/180);
+
+                            var x = radius - Math.cos(directionRadian) * currentRadius;
+                            var y = radius - Math.sin(directionRadian) * currentRadius;
+
+                            var line = paper.line(lastX, lastY, x, y);
+                            line.attr({
+                                stroke: "#f00",
+                                strokeWidth: 2
+                            });
+
+                            lastX = x;
+                            lastY = y;
+                        }
+                    });
+                }
+            }
+        }
     });
 
-function StationsController($scope, $http) {
+app.controller('StationsController', ['$scope', '$http', function($scope, $http) {
+    $scope.snapOptions = {
+        disable: 'right'
+    };
     $scope.list = function () {
         var params = {};
         params.search = $scope.query;
@@ -44,28 +96,30 @@ function StationsController($scope, $http) {
             })
     }
     $scope.list();
-}
+}]);
 
-function StationController($scope, $http) {
+app.controller('StationController', ['$scope', '$http', function($scope, $http) {
     $scope.getHistoric = function () {
-        $http({method: 'GET', url: '/api/2/stations/' + $scope.station._id + '/historic/'}).
+        $scope.historic = [];
+        $http({method: 'GET', url: '/api/2/stations/' + $scope.station._id + '/historic?duration=3600'}).
             success(function (data) {
-                var chart = '';
+                $scope.historic = data;
+
+                var miniChartData = '';
                 var count = data.length;
                 for (var i = count - 1; i >= 0; i--) {
-                    chart += data[i]['_id'] + ':' + data[i]['w-avg'];
+                    miniChartData += data[i]['_id'] + ':' + data[i]['w-avg'];
                     if (i > 0) {
-                        chart += ',';
+                        miniChartData += ',';
                     }
                 }
-                $scope.historic = chart;
-
+                $scope.miniChartData = miniChartData;
             })
-    }
-    $scope.getHistoric()
-}
+    };
+    $scope.getHistoric();
+}]);
 
-function MapController($scope, $http) {
+app.controller('MapController', ['$scope', '$http', function($scope, $http) {
     $scope.center = {
             latitude: 0,
             longitude: 0
@@ -85,4 +139,4 @@ function MapController($scope, $http) {
             });
     }
     $scope.list();
-}
+}]);

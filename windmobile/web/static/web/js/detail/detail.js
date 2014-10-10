8373ad64459482e7981d62eff9f9a5055d6868b4
Yann <yann.savary@gmail.com>
2014-10-10 17:58:33 +0200

    Wind chart with highcharts

--- windmobile/web/static/web/js/detail/detail.js
+++ windmobile/web/static/web/js/detail/detail.js
@@ -36,6 +36,71 @@ angular.module('windMobile.detail', ['ngRoute'])
                 })
         };
 
+        $scope.windChart = function () {
+            $http({method: 'GET', url: '/api/2/stations/' + $scope.stationId + '/historic?duration=172800'}).
+                success(function (data) {
+                    var windAvgSerie = {
+                        name: 'windAvg',
+                        type: 'areaspline',
+                        color: {
+                            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                            stops: [
+                                [0, '#a7a9cb'],
+                                [1, '#252ccb']
+                            ]
+                        },
+                        lineWidth: 2,
+                        lineColor: '#ffffff',
+                        marker: {
+                            enabled: false
+                        },
+                        data: []
+                    };
+                    var windMaxSerie = {
+                        name: 'windMax',
+                        type: 'spline',
+                        color: '#e32d2d',
+                        lineWidth: 2,
+                        marker: {
+                            enabled: false
+                        },
+                        data: []
+                    };
+                    var count = data.length;
+                    for (var i = count - 1; i >= 0; i--) {
+                        var date = data[i]['_id'] * 1000;
+                        windMaxSerie.data.push([date, data[i]['w-max']]);
+                        windAvgSerie.data.push([date, data[i]['w-avg']]);
+                    }
+                    $('#chart').highcharts({
+                        title: {
+                            text: 'Wind speed'
+                        },
+                        legend: {
+                            enabled: false
+                        },
+                        chart: {
+                            backgroundColor: null
+                        },
+                        plotOptions: {
+                            series: {
+                                animation: false
+                            }
+                        },
+                        xAxis: {
+                            type: 'datetime'
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'km/h'
+                            }
+                        },
+                        series: [windAvgSerie, windMaxSerie]
+                    });
+                });
+        };
+
         $scope.getData();
         $scope.getHistoric();
+        $scope.windChart();
     }]);

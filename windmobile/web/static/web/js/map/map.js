c68f8b1d200380c19b507622d5e1f7d844c5663c
Yann Savary <yann.savary@epyx.ch>
2015-03-31 16:29:23 +0200

    Added wind direction markers

--- windmobile/web/static/web/js/map/map.js
+++ windmobile/web/static/web/js/map/map.js
@@ -24,52 +24,86 @@ angular.module('windMobile.map', ['ngRoute', 'ngMap'])
         function displayMarkers(stations) {
             clearOverlays();
 
+            //var element = $compile('<div class="station-card" ng-include src="\'/static/web/templates/_infobox.html\'" ng-controller="StationController"></div>')($scope);
+            var content = '<div class="station-card transparent-background">' +
+                '<div class="title">[[ station.name ]]</div>' +
+                '<div class="last-update">[[ station.last._id|fromNow ]]</div>' +
+                '<div class="altitude">[[ station.alt ]] meters</div>' +
+                '<div class="wind-section">' +
+                '<div class="wind-avg">[[ station.last[\'w-avg\'].toFixed(1) ]]</div>' +
+                '<div class="wind-max">[[ station.last[\'w-max\'].toFixed(1) ]]</div>' +
+                '<div class="unit">km/h</div>' +
+                //'<mini-chart id="mini-chart" data="[[ miniChartData ]]"></mini-chart>' +
+                '</div>' +
+                '<svg id="wind-direction" obj="" wind-direction></svg>' +
+                '</div>';
+
             for (var i = 0; i < stations.length; i++) {
                 var station = stations[i];
 
-                var position = new google.maps.LatLng(station.loc.coordinates[1], station.loc.coordinates[0]);
-                var marker = new google.maps.Marker({
-                    title: station["short"],
-                    position: position,
-                    map: $scope.map
-                });
-                marker.station = station;
-                markersArray.push(marker);
+                if (station.status == "green") {
+                    var position = new google.maps.LatLng(station.loc.coordinates[1], station.loc.coordinates[0]);
+                    var windAverage = station.last['w-avg'];
 
-                //var element = $compile('<div class="station-card" ng-include src="\'/static/web/templates/_infobox.html\'" ng-controller="StationController"></div>')($scope);
-                var content = '<div class="station-card transparent-background">' +
-                    '<div class="title">[[ station.name ]]</div>' +
-                    '<div class="last-update">[[ station.last._id|fromNow ]]</div>' +
-                    '<div class="altitude">[[ station.alt ]] meters</div>' +
-                    '<div class="wind-section">' +
-                    '<div class="wind-avg">[[ station.last[\'w-avg\'].toFixed(1) ]]</div>' +
-                    '<div class="wind-max">[[ station.last[\'w-max\'].toFixed(1) ]]</div>' +
-                    '<div class="unit">km/h</div>' +
-                    //'<mini-chart id="mini-chart" data="[[ miniChartData ]]"></mini-chart>' +
-                    '</div>' +
-                    '<svg id="wind-direction" obj="" wind-direction></svg>' +
-                    '</div>';
-
-                /*
-                 $scope.station = null;
-                 $scope.historic = [];
-                 var element = $compile(content)($scope);
-                 */
-
-                google.maps.event.addListener(marker, 'click', function () {
-                    if (infoBox) {
-                        infoBox.close();
+                    var color;
+                    if (windAverage >= 0 && windAverage < 10) {
+                        color = 'MediumAquaMarine';
+                    } else if (windAverage >= 10 && windAverage < 15) {
+                        color = 'SeaGreen'
+                    } else if (windAverage >= 15 && windAverage < 20) {
+                        color = 'DeepSkyBlue'
+                    } else if (windAverage >= 20 && windAverage < 25) {
+                        color = 'MediumBlue'
+                    } else if (windAverage >= 25 && windAverage < 30) {
+                        color = 'BlueViolet'
+                    } else if (windAverage >= 30 && windAverage < 35) {
+                        color = 'OrangeRed'
+                    } else if (windAverage >= 35) {
+                        color = 'Crimson'
                     }
-                    $scope.station = this.station;
-                    $scope.historic = [];
-                    $scope.getHistoric();
-                    var element = $compile(content)($scope);
-                    $scope.$apply();
-                    infoBox = new InfoBox({
-                        content: element[0]
+
+                    var icon = {
+                        path: 'M21,149.2v86.3L-19,213l50,77l50-77l-40,22.5v-86.3c28.4-4.8,50-29.4,50-59.2S69.4,35.6,41,30.8V-83H21V30.8C-7.4,35.6-29,60.3-29,90S-7.4,144.4,21,149.2z M31,50c22.1,0,40,17.9,40,40s-17.9,40-40,40S-9,112.1-9,90S8.9,50,31,50z',
+                        scale: 0.15,
+                        fillOpacity: 1,
+                        fillColor: color,
+                        strokeColor: color,
+                        strokeWeight: 2,
+                        rotation: (station.last ? station.last['w-dir'] : 0)
+                    };
+
+                    var marker = new google.maps.Marker({
+                        title: station["short"],
+                        position: position,
+                        icon: icon,
+                        map: $scope.map
                     });
-                    infoBox.open($scope.map, this);
-                });
+                    marker.station = station;
+                    markersArray.push(marker);
+
+                    /*
+                     $scope.station = null;
+                     $scope.historic = [];
+                     var element = $compile(content)($scope);
+                     */
+
+                    (function (marker) {
+                        google.maps.event.addListener(marker, 'click', function () {
+                            if (infoBox) {
+                                infoBox.close();
+                            }
+                            $scope.station = marker.station;
+                            $scope.historic = [];
+                            $scope.getHistoric();
+                            var element = $compile(content)($scope);
+                            $scope.$apply();
+                            infoBox = new InfoBox({
+                                content: element[0]
+                            });
+                            infoBox.open($scope.map, marker);
+                        })
+                    })(marker);
+                }
             }
         }
 

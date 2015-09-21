56e3fa2ffe58755ea4e5607ed2de8f6f55556ada
Yann <yann.savary@gmail.com>
2015-09-21 22:27:16 +0200

    Added pip install in update.sh

--- deployment/update.sh
+++ deployment/update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 su - windmobile -c "cd /home/windmobile/WindMobile2-Server; git pull"
+su - windmobile -c "/home/windmobile/.virtualenvs/windmobile-provider/bin/pip install -r /home/windmobile/WindMobile2-Server/providers/requirements.txt"
+su - windmobile -c "/home/windmobile/.virtualenvs/windmobile-django/bin/pip install -r /home/windmobile/WindMobile2-Server/requirements.txt"
 su - windmobile -c "envdir /home/windmobile/WindMobile2-Server/envdir .virtualenvs/windmobile-django/bin/django-admin.py collectstatic --noinput"
 apachectl -k graceful

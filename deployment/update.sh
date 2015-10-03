f57c2bfe354c5211a26ac487b3fcf85970339606
Yann <yann.savary@gmail.com>
2015-10-03 22:14:56 +0200

    npm is only used at compile time

--- deployment/update.sh
+++ deployment/update.sh
@@ -4,5 +4,4 @@ su - windmobile -c "cd /home/windmobile/WindMobile2-Server; git pull"
 su - windmobile -c "/home/windmobile/.virtualenvs/windmobile-provider/bin/pip install -r /home/windmobile/WindMobile2-Server/providers/requirements.txt"
 su - windmobile -c "/home/windmobile/.virtualenvs/windmobile-django/bin/pip install -r /home/windmobile/WindMobile2-Server/requirements.txt"
 su - windmobile -c "envdir /home/windmobile/WindMobile2-Server/envdir .virtualenvs/windmobile-django/bin/django-admin.py collectstatic --noinput"
-su - windmobile -c "cd /home/windmobile/WindMobile2-Server/windmobile/web; npm install"
 apachectl -k graceful

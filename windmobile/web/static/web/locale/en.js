2ba159990f3eaa531ad0c138f3bf1242b21f2a62
Yann <yann.savary@gmail.com>
2015-10-17 00:58:11 +0200

    Fixed help page

--- windmobile/web/static/web/locale/en.js
+++ windmobile/web/static/web/locale/en.js
@@ -20,22 +20,25 @@
         'no recent data': "no recent data",
         'meters': "meters",
         'Unable to find your location': "Unable to find your location",
-        'Red': "Red",
-        'Orange': "Orange",
 
         'HELP_STATION_SUMMARY_TITLE': "Station summary display",
         'HELP_STATION_NAME': "Station name",
         'HELP_ALTITUDE': "Altitude",
         'HELP_LAST_UPDATE': "Date and measure validity",
         'HELP_PROVIDER': "Data provider name",
+        'HELP_STATION_SUMMARY_TEXT_1_RED_DATE': "Red date: ",
         'HELP_STATION_SUMMARY_TEXT_1_RED': ": measures are older than 2 hours or the station is not operational",
+        'HELP_STATION_SUMMARY_TEXT_1_ORANGE_DATE': "Orange date: ",
         'HELP_STATION_SUMMARY_TEXT_1_ORANGE': "measures are between 1 and 2 hours old or the station is not accurate",
         'HELP_STATION_SUMMARY_TEXT_2': "Last mean wind and gust wind measure",
         'HELP_STATION_SUMMARY_TEXT_3': "Last hour mean wind historic",
         'HELP_STATION_SUMMARY_TEXT_4': "Wind direction historic: from center the circle (1 hour ago) " +
         "to outside (last measure)",
-        'HELP_CONTACT_TITLE': "Contact me",
-        'HELP_CONTACT_TEXT': "Questions, issues or improvement ideas ? Please send me an email !"
+        'HELP_PROVIDERS_TITLE': "Data providers list",
+        'HELP_COMPATIBILITY_TITLE': "Compatibility",
+        'HELP_COMPATIBILITY_TEXT': "winds.mobi runs on the latest versions of the major browsers (mobile or desktop)",
+        'HELP_CONTACT_TITLE': "About",
+        'HELP_CONTACT_TEXT': "Questions, issues or improvement ideas ? Please contact me !"
     };
 
     // Node: Export function
--- windmobile/web/static/web/locale/fr.js
+++ windmobile/web/static/web/locale/fr.js
@@ -20,22 +20,25 @@
         'no recent data': "pas de donnée récente",
         'meters': "mètres",
         'Unable to find your location': "Votre position n’a pu être déterminée",
-        'Red': "Rouge",
-        'Orange': "Orange",
 
         'HELP_STATION_SUMMARY_TITLE': "Affichage condensé d'une balise",
         'HELP_STATION_NAME': "Nom de la balise",
         'HELP_ALTITUDE': "Altitude",
         'HELP_LAST_UPDATE': "Date et validité des mesures",
         'HELP_PROVIDER': "Nom du fournisseur des données",
-        'HELP_STATION_SUMMARY_TEXT_1_RED': ": les mesures ont plus de 2 heures ou la balise n'est pas opérationnelle",
-        'HELP_STATION_SUMMARY_TEXT_1_ORANGE': ": les mesures datent d'il y a 1 ou 2 heures ou la balise n'est pas fiable",
-        'HELP_STATION_SUMMARY_TEXT_2': "Dernière mesure du vent moyen et de la plus forte rafale",
-        'HELP_STATION_SUMMARY_TEXT_3': "Historique du vent moyen durant la dernière heure",
+        'HELP_STATION_SUMMARY_TEXT_1_RED_DATE': "Date en rouge: ",
+        'HELP_STATION_SUMMARY_TEXT_1_RED': "les mesures ont plus de 2 heures ou la balise n'est pas opérationnelle",
+        'HELP_STATION_SUMMARY_TEXT_1_ORANGE_DATE': "Date en orange: ",
+        'HELP_STATION_SUMMARY_TEXT_1_ORANGE': "les mesures datent d'il y a 1 ou 2 heures ou la balise n'est pas fiable",
+        'HELP_STATION_SUMMARY_TEXT_2': "Dernière mesure du vent moyen et de la rafale",
+        'HELP_STATION_SUMMARY_TEXT_3': "Tendance: historique du vent moyen durant la dernière heure",
         'HELP_STATION_SUMMARY_TEXT_4': "Historique de la direction du vent: du centre du cercle (il y a 1 heure) " +
         "vers l'extéreur (dernière mesure)",
-        'HELP_CONTACT_TITLE': "Contactez-moi",
-        'HELP_CONTACT_TEXT': "Des questions, problèmes ou idées d'amélioration ? Envoyez-moi un email !"
+        'HELP_PROVIDERS_TITLE': "Liste des fournisseurs de données",
+        'HELP_COMPATIBILITY_TITLE': "Compatibilité",
+        'HELP_COMPATIBILITY_TEXT': "winds.mobi fonctionne sur les dernières versions des principaux navigateurs web (mobile ou bureau)",
+        'HELP_CONTACT_TITLE': "À propos",
+        'HELP_CONTACT_TEXT': "Des questions, problèmes ou idées d'amélioration ? Contactez-moi !"
     };
 
     // Node: Export function

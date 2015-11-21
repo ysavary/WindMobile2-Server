f65df27be20d55927276c5d6108b47fe81908d79
Yann <yann.savary@gmail.com>
2015-11-21 17:50:26 +0100

    Fixed static directory

--- windmobile/settings.py
+++ windmobile/settings.py
@@ -104,6 +104,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'windmobile/static')
+
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "windmobile/web/static"),

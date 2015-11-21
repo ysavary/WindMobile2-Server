93eedd390e260cc9b8fa5bf0fbbba48350e28a28
Yann <yann.savary@gmail.com>
2015-11-21 17:26:39 +0100

    Migrated settings.py to Django 1.8 for django-allauth

--- windmobile/api/urls.py
+++ windmobile/api/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 urlpatterns = patterns(
     '',
-    url(r'^$', RedirectView.as_view(url='/doc/#!/stations')),
+    url(r'^$', RedirectView.as_view(url='/doc/#!/stations', permanent=True)),
     url(r'^stations/$', Stations.as_view(), name='api.stations'),
     url(r'^stations/station_json_doc/$', Doc.as_view(), name='api.station_json_doc'),
     url(r'^stations/(?P<station_id>.+)/historic/$', Historic.as_view(), name='api.station_historic'),

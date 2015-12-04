d6e97eea03924f445245dc8ea97d59cd715e0b84
Yann <yann.savary@gmail.com>
2015-12-04 17:21:09 +0100

    Again...

--- deployment/update.sh
+++ deployment/update.sh
@@ -3,5 +3,5 @@
 su - windmobile -c "cd /home/windmobile/WindMobile2-Server; git fetch; git pull"
 su - windmobile -c "/home/windmobile/.virtualenvs/windmobile-provider/bin/pip install -r /home/windmobile/WindMobile2-Server/providers/requirements.txt"
 su - windmobile -c "/home/windmobile/.virtualenvs/windmobile-django/bin/pip install -r /home/windmobile/WindMobile2-Server/requirements.txt"
-su - windmobile -c "/home/windmobile/.virtualenvs/windmobile-django/bin/django-admin.py collectstatic --noinput"
+su - windmobile -c "/home/windmobile/.virtualenvs/windmobile-django/bin/python /home/windmobile/WindMobile2-Server/manage.py collectstatic --noinput"
 apachectl -k graceful

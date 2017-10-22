c0d7fbff8a037bafe49a18f441e998162291c20f
Yann Savary <yann.savary@gmail.com>
2017-10-22 18:54:45 +0200

    Better log

--- providers/delete_stations.py
+++ providers/delete_stations.py
@@ -12,14 +12,15 @@ parser = argparse.ArgumentParser(description='Delete stations not seen since DAY
 parser.add_argument(
     '--days', type=int, default=60,
     help="Specify the number of days since 'last seen' before deleting the station [default: %(default)s]")
-parser.add_argument('--provider', help="Limit to a provider code, for example 'jdc'")
+parser.add_argument('--provider', help="Delete only stations from this 'provider', for example 'jdc'")
 args = vars(parser.parse_args())
 
 uri = uri_parser.parse_uri(MONGODB_URL)
 client = MongoClient(uri['nodelist'][0][0], uri['nodelist'][0][1])
 mongo_db = client[uri['database']]
 
-logger.info('Deleting all stations not seen since {days} days...'.format(days=str(args['days'])))
+logger.info("Deleting stations from '{provider}' provider not seen since {days} days...".format(
+    provider=args['provider'] or 'any', days=str(args['days'])))
 
 now = arrow.now().timestamp
 query = {'seen': {'$lt': now - args['days'] * 3600 * 24}}

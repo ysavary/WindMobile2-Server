3758267fccd139fbda48f46d02e58adb4162d86d
Yann Savary <yann.savary@gmail.com>
2018-10-06 14:55:16 +0200

    Sending uncaught exception to sentry, ie MemoryError

--- providers/clusters.py
+++ providers/clusters.py
@@ -33,37 +33,40 @@ X = np.array((x, y))
 X = X.T
 
 
-mongo_bulk = db.stations.initialize_ordered_bulk_op()
-mongo_bulk.find({}).update({'$set': {'clusters': []}})
+try:
+    mongo_bulk = db.stations.initialize_ordered_bulk_op()
+    mongo_bulk.find({}).update({'$set': {'clusters': []}})
 
-for n_clusters in reversed(range_clusters):
+    for n_clusters in reversed(range_clusters):
 
-    model = AgglomerativeClustering(linkage='ward', connectivity=None, n_clusters=n_clusters)
-    labels = model.fit_predict(X)
+        model = AgglomerativeClustering(linkage='ward', connectivity=None, n_clusters=n_clusters)
+        labels = model.fit_predict(X)
 
-    for label in range(len(np.unique(labels))):
-        cluster_assign = labels == label
-        cluster = X[cluster_assign]
+        for label in range(len(np.unique(labels))):
+            cluster_assign = labels == label
+            cluster = X[cluster_assign]
 
-        average = np.average(cluster, 0)
-        middle = cluster[KDTree(cluster).query(average)[1]]
+            average = np.average(cluster, 0)
+            middle = cluster[KDTree(cluster).query(average)[1]]
 
-        indexes = np.where((X == middle).all(axis=1))[0]
-        if len(indexes) > 1:
-            stations = list(db.stations.find({'_id': {
-                '$in': [ids[index] for index in indexes.tolist()]
-            }}, {'last._id': 1}))
-            values = {station['_id']: station.get('last', {}).get('_id', 0) for station in stations}
-            station_id = max(values.keys(), key=(lambda k: values[k]))
-            if values[station_id] != 0:
-                log.warning("Multiple 'middle' found, '{station}' has the latest measure".format(station=station_id))
+            indexes = np.where((X == middle).all(axis=1))[0]
+            if len(indexes) > 1:
+                stations = list(db.stations.find({'_id': {
+                    '$in': [ids[index] for index in indexes.tolist()]
+                }}, {'last._id': 1}))
+                values = {station['_id']: station.get('last', {}).get('_id', 0) for station in stations}
+                station_id = max(values.keys(), key=(lambda k: values[k]))
+                if values[station_id] != 0:
+                    log.warning("Multiple 'middle' found, '{station}' has the latest measure".format(station=station_id))
+                else:
+                    log.warning("Ignoring '{cluster}', stations have no measures".format(cluster=ids[cluster_assign]))
+                    continue
+                index = np.where(ids == station_id)[0][0]
             else:
-                log.warning("Ignoring '{cluster}', stations have no measures".format(cluster=ids[cluster_assign]))
-                continue
-            index = np.where(ids == station_id)[0][0]
-        else:
-            index = indexes[0]
-        log.info('{n}: {cluster} -> {median}'.format(n=n_clusters, cluster=ids[cluster_assign], median=ids[index]))
-        mongo_bulk.find({'_id': ids[index]}).update({'$addToSet': {'clusters': int(n_clusters)}})
+                index = indexes[0]
+            log.info('{n}: {cluster} -> {median}'.format(n=n_clusters, cluster=ids[cluster_assign], median=ids[index]))
+            mongo_bulk.find({'_id': ids[index]}).update({'$addToSet': {'clusters': int(n_clusters)}})
 
-mongo_bulk.execute()
+    mongo_bulk.execute()
+except Exception as e:
+    log.exception("Error while creating clusters: {0}".format(e))

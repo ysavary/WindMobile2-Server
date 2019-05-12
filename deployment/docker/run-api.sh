d9221ebe805efbcebb524762cffb0756cd8fcd75
Yann Savary <yann.savary@gmail.com>
2019-05-12 18:19:19 +0200

    Typo

--- deployment/docker/run-api.sh
+++ deployment/docker/run-api.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker run -d -p 8001:8000 -e MONGODB_URL=mongodb://172.17.42.1:27017/windmobile -e SENTRY_URL={SENTRY_DSN} \
--e ENVIRONMENT=production -e OPENAPI_PREFIX=/api/2.1 --restart always --name=winds-mobi-api windsmobi/winds-mobi-api
+-e ENVIRONMENT=production -e OPENAPI_PREFIX=/api/2.1 --restart=always --name=winds-mobi-api windsmobi/winds-mobi-api
